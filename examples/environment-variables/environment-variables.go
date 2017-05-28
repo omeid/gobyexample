9ba5b7146fa220ed56c0d1c91f0c367757dcea1a
O'meid <public@omeid.me>
2017-05-28 11:01:07 +1000

    examples:environment-variables: Fix bug
    Using `strings.Split` will result in incorrect values for when the value containers an equal sign. It is better to use SplitN with n=2.

--- examples/environment-variables/environment-variables.go
+++ examples/environment-variables/environment-variables.go
@@ -16,16 +16,17 @@ func main() {
     // an empty string if the key isn't present in the
     // environment.
     os.Setenv("FOO", "1")
+    os.Setenv("OPT", "bug=free")
     fmt.Println("FOO:", os.Getenv("FOO"))
     fmt.Println("BAR:", os.Getenv("BAR"))
 
     // Use `os.Environ` to list all key/value pairs in the
     // environment. This returns a slice of strings in the
-    // form `KEY=value`. You can `strings.Split` them to
+    // form `KEY=value`. You can `strings.SplitN` them to
     // get the key and value. Here we print all the keys.
     fmt.Println()
     for _, e := range os.Environ() {
-        pair := strings.Split(e, "=")
-        fmt.Println(pair[0])
+        pair := strings.SplitN(e, "=", 2)
+        fmt.Println(pair[0], pair[1])
     }
 }
